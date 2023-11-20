e46c90aab3645c72f5151153ebd9c3ca5aeb82d2
ivanmarinoff <marinoffivan@gmail.com>
2023-11-20 12:14:48 +0200

    Added static files

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const http = require('http');
-const port = 8080;
+const port = 3000;
 const express = require('express');
 const app = express();
+const path = require('path')
 
-// Serve static files from the "static" directory
-app.use(express.static('static'));
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+app.set('view engine', 'pug');
+app.use(require('body-parser')
+  .urlencoded({extended:true}))
+
+
+
 
 // Set up your index route
 app.get('/', function (req, res) {
