b3d234ca1abe561d615354986ffd42fb4d346210
ivanmarinoff <marinoffivan@gmail.com>
2024-02-11 10:59:21 +0200

    Added tagscripts

---
+++ static/js/body_tagscript.js
@@ -0,0 +1,8 @@
+const googleTagManagerNoScript = `
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W53HSSM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+`;
+
+// You can then use googleTagManagerNoScript as needed in your tagscript.js file
