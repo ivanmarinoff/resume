67c72b4898bc79d818cb43af3db2de4e09f21aef
ivanmarinoff <marinoffivan@gmail.com>
2024-02-13 11:20:09 +0200

    Add ga_tag.js file

---
+++ static/js/ga_tag.js
@@ -0,0 +1,9 @@
+// ga_tag.js
+
+// Google tag (gtag.js)
+(function() {
+    const gtag = window.gtag || function () {
+    };
+    gtag('js', new Date());
+    gtag('config', 'G-G3NN486JFL');
+})();
