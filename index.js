58919d2d3f327e51575bad5e2dfe82240169bbf2
ivanmarinoff <marinoffivan@gmail.com>
2024-11-19 08:12:24 +0200

    Updated urls

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const path = require('path');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 const useragent = require('express-useragent');
 const helmet = require('helmet');
 const crypto = require('crypto');
@@ -44,59 +44,59 @@ app.use(
     })
 );
 // Nodemailer setup with custom host and port
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST, // SMTP server (e.g., smtp.gmail.com)
-    port: parseInt(process.env.EMAIL_PORT), // Convert string to number
-    secure: parseInt(process.env.EMAIL_PORT) === 465, // true for port 465 (SSL), false otherwise (e.g., 587 for TLS)
-    auth: {
-        user: process.env.EMAIL_USER, // Your email address
-        pass: process.env.EMAIL_PASS  // Your email password or app-specific password
-    }
-});
+// const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST, // SMTP server (e.g., smtp.gmail.com)
+//     port: parseInt(process.env.EMAIL_PORT), // Convert string to number
+//     secure: parseInt(process.env.EMAIL_PORT) === 465, // true for port 465 (SSL), false otherwise (e.g., 587 for TLS)
+//     auth: {
+//         user: process.env.EMAIL_USER, // Your email address
+//         pass: process.env.EMAIL_PASS  // Your email password or app-specific password
+//     }
+// });
 
 // Function to send an email notification
-async function sendEmail(visitorIP, browser, os, time) {
-    let ipAddress = '';
-
-    async function fetchIpAddress() {
-        try {
-            const response = await fetch('https://api.ipify.org?format=json');
-            const data = await response.json();
-            ipAddress = data.ip;
-        } catch (error) {
-            console.error('Failed to fetch IP address:', error);
-        }
-    }
-
-    // Fetch IP address on page load
-    await fetchIpAddress();
-
-    // fetch('https://api.ipify.org?format=json')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         ipAddress = data.ip;
-    //     });
-
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: process.env.EMAIL_USER1, // Sending the email to yourself
-        subject: 'A new visitor has accessed your page.',
-        text: `
-            A visitor has left your page.
-            IP: ${visitorIP}
-            Browser: ${browser}
-            OS: ${os}
-            Session Ended: ${time}
-        `
-    };
-
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent:', info.response);
-    } catch (error) {
-        console.log('Error sending email:', error);
-    }
-}
+// async function sendEmail(visitorIP, browser, os, time) {
+//     let ipAddress = '';
+//
+//     async function fetchIpAddress() {
+//         try {
+//             const response = await fetch('https://api.ipify.org?format=json');
+//             const data = await response.json();
+//             ipAddress = data.ip;
+//         } catch (error) {
+//             console.error('Failed to fetch IP address:', error);
+//         }
+//     }
+//
+//     // Fetch IP address on page load
+//     await fetchIpAddress();
+//
+//     // fetch('https://api.ipify.org?format=json')
+//     //     .then(response => response.json())
+//     //     .then(data => {
+//     //         ipAddress = data.ip;
+//     //     });
+//
+//     const mailOptions = {
+//         from: process.env.EMAIL_USER,
+//         to: process.env.EMAIL_USER1, // Sending the email to yourself
+//         subject: 'A new visitor has accessed your page.',
+//         text: `
+//             A visitor has left your page.
+//             IP: ${visitorIP}
+//             Browser: ${browser}
+//             OS: ${os}
+//             Session Ended: ${time}
+//         `
+//     };
+//
+//     try {
+//         const info = await transporter.sendMail(mailOptions);
+//         console.log('Email sent:', info.response);
+//     } catch (error) {
+//         console.log('Error sending email:', error);
+//     }
+// }
 
 const isDesktop = (userAgent) => {
     return /Windows|Macintosh|Linux/.test(userAgent);
@@ -186,13 +186,13 @@ app.get('/manifest.json', function (req, res) {
 
 // Set up your index route
 app.get('/', function (req, res) {
-    const visitorIP = req.ip; // Get the visitor's IP address
-    const browser = req.useragent.browser; // Get the browser information
-    const os = req.useragent.os; // Get the OS information
-    const time = new Date().toLocaleString(); // Get the current time
+    // const visitorIP = req.ip; // Get the visitor's IP address
+    // const browser = req.useragent.browser; // Get the browser information
+    // const os = req.useragent.os; // Get the OS information
+    // const time = new Date().toLocaleString(); // Get the current time
 
     // Send an email notification with all details
-    sendEmail(visitorIP, browser, os, time);
+    // sendEmail(visitorIP, browser, os, time);
 
     // Serve the HTML file
     res.sendFile(__dirname + '/index.html');
